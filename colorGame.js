06d39d376a80ed0324a8a4622d421ab643434587
sooyoungs0120@gmail.com <sooyoungs0120@gmail.com>
2019-04-16 22:10:55 +0900

    resetButton & h1 color changed

--- colorGame.js
+++ colorGame.js
@@ -1,16 +1,25 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-];
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener('click', function() {
+    // alert('clicked');
+    // generate all new colors
+    colors = generateRandomColors(6);
+    // pick a new random color from array
+    pickedColor = pickColor();
+    // change colorDisplay to match picked Color
+    colorDisplay.textContent = pickedColor;
+    // change color of squares
+    for(var i = 0; i < squares.length; i++) {
+        squares[i].style.background = colors[i];
+    }
+    h1.style.background = "#232323";
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -30,10 +39,13 @@ for(var i = 0; i < squares.length; i++) {
         // compare color to pickedColor
         if(clickedColor === pickedColor) {
            messageDisplay.textContent = "Correct ;)";
-           // 맞으면 맞는 색깔로 사각형들 다 칠해지기
+           resetButton.textContent = "Play Again?"
+           // corrected -> all paint with correct color
            changeColors(clickedColor);
+           h1.style.background = clickedColor;
         } else {
-            // 틀린 사각형 지우기(배경색이랑 똑같이 만들어버려서 안보이게끔)
+            // wrong -> clicked square clear
+            // no delete! just make the background color the same as invisible
             this.style.background = "#232323";
             messageDisplay.textContent = "Try Again";
         }
@@ -52,4 +64,30 @@ function pickColor() {
     var random = Math.floor(Math.random() * colors.length);
     console.log(random);
     return colors[random];
+}
+
+function generateRandomColors(num) {
+    // make an array
+    var arr = [];
+    // add num random colors to array
+    for (var i = 0; i < num; i++) {
+        // get random color and push into arr
+        arr.push(randomColor());
+    }
+    // return that array
+    return arr;
+}
+
+function randomColor() {
+    // pick a "red" from 0 -255
+    var r = Math.floor(Math.random() * 256);
+    // pick a "green" from 0 -255
+    var g = Math.floor(Math.random() * 256);
+    // pick a "blue" from 0 -255
+    var b = Math.floor(Math.random() * 256);
+
+    // Watch out! ---> "space"
+    // "rgb(r, g, b)"
+    // not "rgb(r,g,b)"
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
